e63919047dca20ab11065e8b8383bc1e8c63c89e
ApramPepo <120373555+ApramPepo@users.noreply.github.com>
2025-05-01 22:38:17 +0300

    Progress update

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 This is a small QR project
 
+A small JS project that is built using small functions and DOM manipulation. <br>
+It takes any text or URL input and processes it to create a QR-code as an output.
+<hr>
+<br>
+Progress : <br>
+1- Functional App = ❌ <br>
+2- Ability to download QR = ❌ <br>
+3- Responsive Web Design = ❌ <br>
+4- Responsive Mobile Design = ❌
--- main.js
+++ main.js
@@ -1,1 +1,34 @@
-console.log("Lol");
+let qrCode = null;
+let qrData = null;
+
+function generateQR() {
+    const url = document.getElementById('urlInput').value.trim();
+    const qrpara = document.getElementById('qrcode');
+
+    if (!url) {
+        alert("Please enter Url or Text");
+        return;
+    }
+
+    qrpara.innerText = "";
+    qrCode = new QRCode(qrpara, {
+        text: url,
+        width: 200,
+        height: 200,
+        colorDark: "#000000",
+        colorLight: "#ffffff",
+        correctLevel: QRCode.CorrectLevel.H
+    });
+
+    document.getElementById('download').style.display = 'inline-block';
+
+}
+
+function downloadQR() {
+    const canvas = document.querySelector('#qrcode canvas');
+
+    if (!canvas) {
+        alert("No QR code to download");
+        return;
+    }
+}
